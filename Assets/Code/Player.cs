96153640fdb44435c7697bb6d6c43d092f16d200
MJ Sidik <darkimanity@gmail.com>
2024-07-27 10:33:46 +0700

    Add Jumping Control

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] LayerMask groundLayer;
+
     Rigidbody2D rb;
     Animator anim;
     bool facingRight = true;
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +25,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // Use FixedUpdate for physics related operations
     }
+
     void FixedUpdate()
     {
         float dirX = Input.GetAxis("Horizontal");
@@ -33,6 +41,13 @@ public class Player : MonoBehaviour
         else
         {
             anim.SetBool("IsMove", false);
+        }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
 
         FlipCharacter(dirX);
@@ -49,6 +64,16 @@ public class Player : MonoBehaviour
         {
             facingRight = false;
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    // Method to draw gizmos in the editor
+    void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
     }
 }
